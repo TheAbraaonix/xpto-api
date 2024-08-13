c5214d93b9a6a24801b1e1c953729f7a282cd3d4
Carlos Abraão <carlosabraao@hotmail.com.br>
2024-08-13 15:04:28 -0300

    implementing repository layer

---
+++ XptoAPI/Repositories/IServiceOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using XptoAPI.Models;
+
+namespace XptoAPI.Repositories
+{
+    public interface IServiceOrderRepository
+    {
+        Task<IEnumerable<ServiceOrder>> GetAllServiceOrderAsync();
+        Task<ServiceOrder> GetServiceOrderByIdAsync(Guid id);
+        Task<ServiceOrder> CreateServiceOrderAsync(ServiceOrder orderService);
+        Task<ServiceOrder> UpdateServiceOrderAsync(ServiceOrder orderService);
+        Task<ServiceOrder> DeleteServiceOrderAsync(ServiceOrder orderService);
+    }
+}
---
+++ XptoAPI/Repositories/ServiceOrderRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using XptoAPI.Context;
+using XptoAPI.Models;
+
+namespace XptoAPI.Repositories
+{
+    public class ServiceOrderRepository : IServiceOrderRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ServiceOrderRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ServiceOrder>> GetAllServiceOrderAsync()
+        {
+            IEnumerable<ServiceOrder> serviceOrders = await _context.serviceOrders.ToListAsync();
+            return serviceOrders;
+        }
+
+        public async Task<ServiceOrder> GetServiceOrderByIdAsync(Guid id)
+        {
+            ServiceOrder? serviceOrder = await _context.serviceOrders.FirstOrDefaultAsync(so => so.Id == id);
+            return serviceOrder;
+        }
+
+        public async Task<ServiceOrder> CreateServiceOrderAsync(ServiceOrder orderService)
+        {
+            _context.serviceOrders.Add(orderService);
+            await _context.SaveChangesAsync();
+            return orderService;
+        }
+
+        public async Task<ServiceOrder> UpdateServiceOrderAsync(ServiceOrder orderService)
+        {
+            _context.Set<ServiceOrder>().Update(orderService);
+            await _context.SaveChangesAsync();
+            return orderService;
+        }
+
+        public async Task<ServiceOrder> DeleteServiceOrderAsync(ServiceOrder orderService)
+        {
+            _context.Remove(orderService);
+            await _context.SaveChangesAsync();
+            return orderService;
+        }
+    }
+}
