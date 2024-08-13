92f0dc77cbb4e3a8779432491cb21b240563d237
Carlos Abraão <carlosabraao@hotmail.com.br>
2024-08-13 14:15:46 -0300

    adding connection to MySQL database

--- XptoAPI/Program.cs
+++ XptoAPI/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using XptoAPI.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DevConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
